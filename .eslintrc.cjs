05f0d412fd6409338d6666d802971caf8ed53bea
yutak23 <yutapilafu@gmail.com>
2023-11-14 14:52:20 +0900

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+module.exports = {
+	root: true,
+	extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+	rules: {
+		'import/no-extraneous-dependencies': ['warn', { packageDir: './' }]
+	},
+	overrides: [
+		{
+			files: ['./**/*.{ts,tsx,cts}'],
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'airbnb-typescript/base',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'prettier'
+			],
+			parser: '@typescript-eslint/parser',
+			parserOptions: { project: './tsconfig.eslint.json' },
+			plugins: ['@typescript-eslint'],
+			rules: {
+				'import/no-extraneous-dependencies': ['warn', { packageDir: './' }]
+			}
+		}
+	],
+	ignorePatterns: ['dist', 'node_modules']
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+## Serverless MySQL HTTP (SMH)
