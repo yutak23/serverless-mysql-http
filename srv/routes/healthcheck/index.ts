cada4e2afeeae9aba044db00884401809ac2c472
yutak23 <yutapilafu@gmail.com>
2023-11-20 13:45:56 +0900

    chore: enable E2E testing with ci

---
+++ srv/routes/healthcheck/index.ts
@@ -0,0 +1,19 @@
+import type { Application, Request, Response, NextFunction } from 'express';
+import { strict as assert } from 'assert';
+import type { ExpressHandlerError } from '../../types/express-handler.js';
+
+import healthcheck from './v1/healthcheck.js';
+
+const BASE_V1_API_PATH = `/healthcheck`;
+
+export default (options: { app?: Application } = {}) => {
+	assert.ok(options.app, 'app must be required');
+	const { app } = options;
+
+	app.use(`${BASE_V1_API_PATH}`, healthcheck);
+
+	// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+	app.use((err: ExpressHandlerError, _req: Request, res: Response, _next: NextFunction) => {
+		res.status(err?.status || 500).error(err);
+	});
+};
---
+++ srv/routes/healthcheck/v1/healthcheck.ts
@@ -0,0 +1,26 @@
+import type { Request, RequestHandler, Response } from 'express';
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const router = express.Router();
+
+router.get('', (async (_req: Request, res: Response) => {
+	const config = {
+		host: process.env['MYSQL_HOST'] || '127.0.0.1',
+		port: Number(process.env['MYSQL_PORT']) || 3306,
+		user: process.env['MYSQL_USER'] || 'root',
+		password: process.env['MYSQL_PASSWORD'] || '',
+		database: process.env['MYSQL_DATABASE'] || 'for_vitest',
+		timezone: 'UTC' // Planetscale timezone is UTC
+	};
+
+	try {
+		const connection = await mysql.createConnection(config);
+		await connection.ping();
+		return res.status(200).json({ message: 'OK' });
+	} catch (error) {
+		return res.status(500).error(error as Error);
+	}
+}) as RequestHandler);
+
+export default router;
